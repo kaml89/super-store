5bd222c3404c5a2b73fd9467737ba40e857725b8
kaml89 <rba.kamil@gmail.com>
2021-05-08 16:02:27 +0200

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,16 @@
+
+
+const Navbar = () => {
+    return (
+        <nav class='navbar'>
+            <div class='logo'>Super Store</div>
+            <ul>
+                <li class='nav-item'></li>
+                <li class='nav-item'></li>
+                <li class='nav-item'></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
