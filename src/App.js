0d960d5f57604d4cf075f82674564828bead9ca8
kaml89 <rba.kamil@gmail.com>
2021-05-09 14:45:17 +0200

    add navigation using react-router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
 import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Cart from './components/Cart'
+import Deals from './components/Deals'
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
+      <Router>
+        <Navbar />
+        <Switch>
+          <Route exact path='/' component={ Home }/>
+          <Route path='/deals' component={ Deals }/>
+          <Route path='/cart' component={ Cart }/>
+        </Switch>
+      </Router>
+      
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
-
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <nav class='navbar'>
-            <div class='logo'>Super Store</div>
-                <div class='nav'>
-                    <div class='nav-item'>Home</div>
-                    <div class='nav-item'>Deals</div>
-                    <div class='nav-item'>Cart</div>
-                    <button class='nav-item signin-btn'>Sign In</button>
+        <nav className='navbar'>
+            <div className='logo'>Super Store</div>
+                <div className='nav'>
+                
+                    <div className='nav-item'><NavLink to={'/'} exact activeClassName='selected'>Home</NavLink></div>
+                    <div className='nav-item'><NavLink to={'/deals'} activeClassName='selected'>Deals</NavLink></div>
+                    <div className='nav-item'><NavLink to={'/cart'} activeClassName='selected'>Cart</NavLink></div>
+                    <button className='nav-item signin-btn'>Sign In</button>
                 </div>
                 
         </nav>
