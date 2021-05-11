614e8fab6033aa9b2e774a714beee64c2d237419
kaml89 <rba.kamil@gmail.com>
2021-05-11 23:15:20 +0200

    add service for loading data from api

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,20 @@
-const Home = () => {
+import React, { useState, useEffect } from 'react';
+import itemService from '../services/item'
+
+import axios from 'axios';
+
+const Home = (props) => {
+
+    const [ items, setItems ] = useState([]);
+
+    useEffect(async () => {
+        
+        const response = await itemService.get();
+        console.log(response.items)
+        setItems(response.items)
+        
+    }, []);
+    
     return (
         <div>Home</div>
     );
