bfd8af1b473082186e7931f9fb9bdc3e820514ba
kaml89 <rba.kamil@gmail.com>
2021-05-24 17:34:32 +0200

    update Item page

--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -1,9 +1,59 @@
-import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import { useParams, withRouter } from "react-router";
+import itemService from '../../services/item';
+import RatingStars from '../Stars/RatingStars';
+import Notification from '../Notification/Notification'
+import './Item.css'
 
 const Item = () => {
+    
     const { itemId } = useParams();
+    const [ item, setItem ] = useState({})
+    const [ message, setMessage ] = useState(null)
+
+    const handleChange = e => {
+        if (e.target.value > item.stockCount) {
+            setMessage('Not enough items in stock');
+        } else if (e.target.value < 0) {
+            setMessage("Quantity number can't be negative")
+        } else {
+            setMessage(null);
+        }
+
+    };
+
+    useEffect(() => {
+        const fetchItem = async () => {
+            console.log(1)
+            const response = await itemService.getById(itemId);
+            setItem(response);
+            console.log(response);
+        }
+        fetchItem();
+    }, []);
+
+
     return (
-        <div>{itemId}</div>
+        <div className='container'>
+                {console.log(3)}
+
+            <div className='product-img'>
+                <img src={ item.imageUrl }/>
+            </div>
+            <div className='info'>
+                <h2 className='name'>{ item.name }</h2>
+                <RatingStars avgRating = { item.avgRating }/>
+                <p className='description'>{ item.description }</p>
+                <h3 className='price'>{ item.price }</h3>
+                <form>
+                    Quantity: <input type="number" className='quantity' min="1" max={ item.stockCount } onChange={ handleChange }/>
+                    <button>Add to Cart</button>
+                </form>
+                <p>{message}</p>
+                {/* <Notification message={ message }/> */}
+                
+            </div>
+        </div>
     );
 };
 
