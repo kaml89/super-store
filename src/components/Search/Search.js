b16a416225d3c302a26d6abdca81f556646e0f38
kaml89 <rba.kamil@gmail.com>
2021-05-28 22:28:51 +0200

    add clear button to Search component

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,23 +1,33 @@
 import './Search.css';
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const Search = ({ handleSearch }) => {
     
     const [ query, setQuery ] = useState('');
+    const queryInput = useRef(null);
 
     const onSearch = (e) => {
         e.preventDefault();
         handleSearch(query);
     }
+
+    const handleClear = (e) => {
+        e.preventDefault();
+        setQuery('');
+        handleSearch(query);
+        queryInput.current.value = '';
+    };
     
 
     return (
         <div className='search-bar'>
             <form onSubmit= { onSearch }>
                 <input type="text" 
+                       ref={ queryInput }
                        onChange={ (e) => setQuery(e.target.value) } />
                 <button>search</button>
+                <button onClick={ handleClear }>X</button>
             </form>
         </div>
     );
