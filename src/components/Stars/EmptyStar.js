7398fed5f3f93a08f39e376b7188e175d6f08d17
kaml89 <rba.kamil@gmail.com>
2021-06-22 19:39:16 +0200

    fix alt img warning

--- src/components/Stars/EmptyStar.js
+++ src/components/Stars/EmptyStar.js
@@ -1,6 +1,6 @@
 const EmptyStar = () => {
     return (
-        <img src = 'https://www.guidedprojects.dev/assets/files/star-empty-849812a23c4515e156571518674ae723.svg'/>
+        <img src = 'https://www.guidedprojects.dev/assets/files/star-empty-849812a23c4515e156571518674ae723.svg' alt=''/>
     );
 }
 
--- src/components/Stars/FullStar.js
+++ src/components/Stars/FullStar.js
@@ -1,6 +1,6 @@
 const FullStar = () => {
     return (
-        <img src = 'https://www.guidedprojects.dev/assets/files/star_full-f757f2a6eb46312ac3b0a9f4923add12.svg'/>
+        <img src = 'https://www.guidedprojects.dev/assets/files/star_full-f757f2a6eb46312ac3b0a9f4923add12.svg' alt=''/>
     );
 }
 
