d89df018a83ec68308f397ca8286e515344cdbcd
kaml89 <rba.kamil@gmail.com>
2021-06-22 18:44:58 +0200

    connect app to cartContext provider

--- src/App.js
+++ src/App.js
@@ -4,21 +4,24 @@ import './App.css';
 import Navbar from './components/Navbar'
 import Home from './components/Home/Home'
 import Cart from './components/Cart'
-import Deals from './components/Deals'
+import Deals from './components/Deals/Deals'
 import Item from './components/Item/Item';
+import { CartProvider } from './context/CartContext'
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={ Home }/>
-          <Route path='/deals' component={ Deals }/>
-          <Route path='/cart' component={ Cart }/>
-          <Route path='/item/:itemId' component={ Item }/>
-        </Switch>
-      </Router>
+      <CartProvider>
+        <Router>
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={ Home }/>
+            <Route path='/deals' component={ Deals }/>
+            <Route path='/cart' component={ Cart }/>
+            <Route path='/item/:itemId' component={ Item }/>
+          </Switch>
+        </Router>
+      </CartProvider>
       
     </div>
   );
