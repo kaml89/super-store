df36fa19b5d7c06d1e40c7ef098a0ead21177d55
kaml89 <rba.kamil@gmail.com>
2021-07-05 14:59:58 +0200

    add loading message before items are fetched

--- src/components/Deals/Deals.js
+++ src/components/Deals/Deals.js
@@ -5,10 +5,13 @@ import ProductsList from '../ProductsList/ProductsList'
 const Deals = () => {
 
     const [ itemsOnSale, setItemsOnSale ] = useState([]);
+    const [ isLoading, setIsLoading ] = useState(false);
 
     const fetchData = async () => {
+        setIsLoading(true);
         const response = await itemService.get({ 'isOnSale': true });
         setItemsOnSale(response.items)
+        setIsLoading(false);
     };
 
     useEffect(() => {
@@ -20,6 +23,7 @@ const Deals = () => {
     return (
         <div>
         {
+            isLoading ? <p>Loading...</p> : 
             itemsOnSale.length !== 0 ? <ProductsList items={itemsOnSale} /> :
                 'Currenty there are no items on sale :('
         }
