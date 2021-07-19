c1a517a07245e644abe3e537a796b86b0aa7265c
kaml89 <rba.kamil@gmail.com>
2021-07-19 17:37:20 +0200

    add bootstrap Container component

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,9 @@
 import { Link } from 'react-router-dom';
 import { useCart } from '../context/CartContext';
+import { useState } from 'react';
 import Navbar from 'react-bootstrap/Navbar'; 
 import Nav from 'react-bootstrap/Nav';
-// import { Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 
 const Navigation = () => {
     const { cart } = useCart();
@@ -14,23 +15,13 @@ const Navigation = () => {
     };
 
     return (
-        // <nav className='navbar'>
-        //     <div className='logo'>Super Store</div>
-        //         <div className='nav'>
-        //             <NavLink to={'/'} exact activeClassName='selected' className='nav-item'>Home</NavLink>
-        //             <NavLink to={'/deals'} activeClassName='selected' className='nav-item'>Deals</NavLink>
-        //             <NavLink to={'/cart'} activeClassName='selected' className='nav-item'>Cart { getTotalCartItems(cart) }</NavLink>
-        //             <button className='nav-item signin-btn'>Sign In</button>
-        //         </div>
-                
-        // </nav>
         <div>
             <Navbar bg="light" expand="lg">
-                {/* <Container> */}
+                <Container>
                     <Navbar.Brand as={Link} to="/" >super store</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="mr-auto">
+                        <Nav className="ms-auto">
                             <Nav.Item>
                                 <Nav.Link as={Link} to="/" >Home</Nav.Link>
                             </Nav.Item>
@@ -40,9 +31,9 @@ const Navigation = () => {
                             <Nav.Item>
                                 <Nav.Link as={Link} to="/cart" >Cart { getTotalCartItems(cart) }</Nav.Link>
                             </Nav.Item>
-                        </Nav>
+                        </Nav>                      
                     </Navbar.Collapse>
-                {/* </Container> */}
+                </Container>
             </Navbar>
         </div>
         
