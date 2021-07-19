3f8efd350c089ae8e6f20939373f6cb38eb6371c
kaml89 <rba.kamil@gmail.com>
2021-07-19 17:38:43 +0200

    move bootstrap import from App.js to index.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Home from './pages/Home/Home'
 import Cart from './pages/Cart/Cart'
 import Deals from './pages/Deals/Deals'
