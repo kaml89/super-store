6c691735483b5974522e98777856da3e574c2326
kaml89 <rba.kamil@gmail.com>
2021-08-02 19:10:23 +0200

    add padding in Cart component

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,6 +1,7 @@
 import { useCart } from '../../context/CartContext';
 import CartItem from '../../components/CartItem/CartItem';
 import { Link } from 'react-router-dom';
+import './Cart.css';
 
 const Cart = () => {
     const { cart, getTotal} = useCart();
@@ -9,13 +10,13 @@ const Cart = () => {
     return (
         <div>
             <h2>Shopping Cart</h2>
-            <ul>
-                { cart.map(item => <li> <CartItem item={ item }/> </li>)}
-            </ul>
-            <h2>${ getTotal() }</h2>
-            <Link to='/checkout'>
-                <button>Checkout</button>
-            </Link>
+            <div className='cart-container'>
+                { cart.map(item => <div className='cart-item'> <CartItem item={ item }/> </div>)}
+                <h2>${ getTotal() }</h2>
+                <Link to='/checkout'>
+                    <button>Checkout</button>
+                </Link>
+            </div>
             
         </div>
         
