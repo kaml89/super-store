6af8224fc3e4a4213027ac8962822daa11764b6f
kaml89 <rba.kamil@gmail.com>
2021-08-13 14:57:02 +0200

    add badge element to cart icon on navigation bar

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-// import { useCart } from '../../context/CartContext';
+import { useCart } from '../../context/CartContext';
 import Navbar from 'react-bootstrap/Navbar'; 
 import Nav from 'react-bootstrap/Nav';
 import { Container } from 'react-bootstrap';
@@ -7,20 +7,20 @@ import './Navigation.css';
 import { ReactComponent as CartIcon } from '../../assets/shopping-cart.svg';
 
 const Navigation = () => {
-    // const { cart } = useCart();
-    // const getTotalCartItems = ( cart ) => {
-    //     return cart
-    //                 .map(item => item.quantity)
-    //                 .reduce(( prev, curr ) => prev + curr, 0);
+    const { cart } = useCart();
+    const getTotalCartItems = ( cart ) => {
+        return cart
+                    .map(item => item.quantity)
+                    .reduce(( prev, curr ) => prev + curr, 0);
 
-    // };
+    };
 
     return (
         <div>
             <Navbar bg="dark" variant='dark' expand="lg">
                 <Container>
-                    <Navbar.Brand as={Link} to="/" >super store</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Brand className='px-2' as={Link} to="/" >super store</Navbar.Brand>
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
                             <Nav.Item>
@@ -31,13 +31,17 @@ const Navigation = () => {
                             </Nav.Item>
                         </Nav>                      
                     </Navbar.Collapse>
-                    <Nav.Item>
-                        <Nav.Link as={Link} to="/cart" >
-                            <div className='cart-icon'>
-                                <CartIcon /> 
-                            </div> 
-                        </Nav.Link>
-                    </Nav.Item>
+                    <Nav className="ms-auto">
+                        <Nav.Item>
+                            <Nav.Link as={Link} to="/cart" >
+                                <div>
+                                    <CartIcon /> 
+                                    <span className={ cart.length > 0 ? 'cart-badge' : 'hidden' }>{ getTotalCartItems(cart) }</span>
+                                </div> 
+                            </Nav.Link>
+                        </Nav.Item>
+                    </Nav>
+                    
                 </Container>
             </Navbar>
         </div>
