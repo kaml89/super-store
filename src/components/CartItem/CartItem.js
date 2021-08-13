562a157a49017578c2eabadbd41cabcce4460b5e
kaml89 <rba.kamil@gmail.com>
2021-08-13 14:54:29 +0200

    change default buttons for Bootstrap buttons

--- src/components/CartItem/CartItem.js
+++ src/components/CartItem/CartItem.js
@@ -1,6 +1,7 @@
 import { useCart } from '../../context/CartContext';
 import PropTypes from 'prop-types';
 import './CartItem.css';
+import { Button } from 'react-bootstrap';
 
 const CartItem = ({ item }) => {
     const { addItem, removeItem } = useCart();
@@ -14,10 +15,10 @@ const CartItem = ({ item }) => {
             <div className='cart-item-info'>
                 <h3 className='cart-item-name'>{ item.name }</h3>
                 <div className='quantity-controller'>
-                    <button onClick={ () => addItem(item)}>+</button>
-                    <button onClick={ () => removeItem(item.id)}>-</button>
-                    <button onClick={ () => removeItem(item.id) }>X</button>
+                    <Button variant="success" onClick={ () => addItem(item) }>+</Button>
                     <p>{ item.quantity }</p>
+                    <Button variant='danger' onClick={ () => removeItem(item.id)}> - </Button>
+                    
                 </div>
             </div>
             <h2 className='cart-item-price'>${ (item.price * item.quantity).toFixed(2) }</h2>
