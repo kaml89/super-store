87acad935fcd0dcb0443983c8200f9c341e17c20
kamil <rba.kamil@gmail.com>
2021-11-15 20:59:18 +0100

    switch from BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import './App.css';
-import Home from './pages/Home/Home'
-import Cart from './pages/Cart/Cart'
-import Deals from './pages/Deals/Deals'
-import Item from './components/Item/Item';
-import Checkout from './pages/Checkout/Checkout';
-import { CartProvider } from './context/CartContext'
-import { NotificationProvider } from './context/NotificationContext';
-import Navigation from './components/Navigation/Navigation';
-import Notification from './components/Notification/Notification';
-
+import React from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home/Home";
+import Cart from "./pages/Cart/Cart";
+import Deals from "./pages/Deals/Deals";
+import Item from "./components/Item/Item";
+import Checkout from "./pages/Checkout/Checkout";
+import { CartProvider } from "./context/CartContext";
+import { NotificationProvider } from "./context/NotificationContext";
+import Navigation from "./components/Navigation/Navigation";
+import Notification from "./components/Notification/Notification";
 
 function App() {
   return (
@@ -21,17 +20,15 @@ function App() {
             <Navigation />
             <Notification />
             <Switch>
-              <Route exact path='/' component={ Home }/>
-              <Route path='/deals' component={ Deals }/>
-              <Route path='/cart' component={ Cart }/>
-              <Route path='/checkout' component={ Checkout }/>
-              <Route path='/item/:itemId' component={ Item }/>
-              
+              <Route exact path="/" component={Home} />
+              <Route path="/deals" component={Deals} />
+              <Route path="/cart" component={Cart} />
+              <Route path="/checkout" component={Checkout} />
+              <Route path="/item/:itemId" component={Item} />
             </Switch>
           </Router>
         </NotificationProvider>
       </CartProvider>
-      
     </div>
   );
 }
