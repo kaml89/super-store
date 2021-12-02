bf9c4732488d4f50f98619d5e8b678276a71ba1f
kamil <rba.kamil@gmail.com>
2021-12-02 17:49:31 +0100

    change code formating

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,42 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import itemService from '../../services/item';
-import ProductsList from '../../components/ProductsList/ProductsList';
-import Search from '../../components/Search/Search'
-import './Home.css'
+import React, { useState, useEffect } from "react";
+import itemService from "../../services/item";
+import ProductsList from "../../components/ProductsList/ProductsList";
+import Search from "../../components/Search/Search";
+import "./Home.css";
 
 const Home = () => {
+  const [items, setItems] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [ items, setItems ] = useState([]);
-    const [ isLoading, setIsLoading ] = useState(false);
+  const fetchData = async (params = {}) => {
+    setIsLoading(true);
+    const response = await itemService.get(params);
+    setItems(response);
+    setIsLoading(false);
+  };
 
-    const fetchData = async (params = {}) => {
-        setIsLoading(true);
-        const response = await itemService.get(params);
-        setItems(response.items);
-        setIsLoading(false);
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData()
-    }, []);
+  const handleSearch = (query) => {
+    fetchData({ q: query });
+  };
 
-
-    const handleSearch = (query) => {
-        fetchData({ 'q': query })
-    };
-    
-    return (
-        <div className='page-container'>
-            { isLoading ? <p>Loading</p> : 
-                <div>
-                    <Search handleSearch={ handleSearch }/>
-                    <ProductsList items = { items }/>
-                </div>
-            }
+  return (
+    <div className="page-container">
+      {isLoading ? (
+        <p>Loading</p>
+      ) : (
+        <div>
+          <Search handleSearch={handleSearch} />
+          <ProductsList items={items} />
         </div>
-        
- 
-    );
-}
+      )}
+    </div>
+  );
+};
 
 export default Home;
