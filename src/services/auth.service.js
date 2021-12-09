f2e142ca9c0f6fac754cf9a0b2ccecfc7eeff985
kamil <rba.kamil@gmail.com>
2021-12-09 21:35:47 +0100

    create auth service

---
+++ src/services/auth.service.js
@@ -0,0 +1,32 @@
+import axios from "axios";
+
+const authAxios = axios.create({
+  baseURL: "https://store-api-k.herokuapp.com/auth",
+});
+
+authAxios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    const code = error && error.response ? error.response.status : 0;
+    if (code >= 400) {
+      console.log("error code", code);
+    }
+    return Promise.reject(error);
+  }
+);
+
+const signup = async (credentials) => {
+  const response = await authAxios.post("/signup", credentials);
+  return response.data;
+};
+
+const login = async (credentials) => {
+  const response = await authAxios.post("/login", credentials);
+  return response.data;
+};
+
+const authService = { signup, login };
+
+export default authService;
