6fb065002c5c1cbda6d3524ec51e7c10077dd1d1
kamil <rba.kamil@gmail.com>
2021-12-09 21:33:53 +0100

    add loading spinner

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,22 @@
-import './Button.css';
+import "./Button.css";
+import { Spinner } from "react-bootstrap";
 
-const Button = ({ label, onClick}) => {
-
-    return (
-        <button 
-            onClick = { onClick }
-            className = 'button'>
-                { label }
-        </button>
-    );
-}
+const Button = ({ label, onClick, isLoading = false }) => {
+  return (
+    <button onClick={onClick} className="button">
+      {isLoading ? (
+        <Spinner
+          as="span"
+          animation="border"
+          size="sm"
+          role="status"
+          aria-hidden="true"
+        />
+      ) : (
+        <span>{label}</span>
+      )}
+    </button>
+  );
+};
 
 export default Button;
