57ab55dc1929645ec59d65daa6745ef57559a0b1
kamil <rba.kamil@gmail.com>
2021-12-22 13:36:18 +0100

    change indentations

--- src/pages/Deals/Deals.js
+++ src/pages/Deals/Deals.js
@@ -1,34 +1,35 @@
 import { useEffect, useState } from "react";
-import itemService from '../../services/item'; 
-import ProductsList from '../../components/ProductsList/ProductsList'
+import itemService from "../../services/item.service";
+import ProductsList from "../../components/ProductsList/ProductsList";
 
 const Deals = () => {
+  const [itemsOnSale, setItemsOnSale] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-    const [ itemsOnSale, setItemsOnSale ] = useState([]);
-    const [ isLoading, setIsLoading ] = useState(false);
+  const fetchData = async () => {
+    setIsLoading(true);
+    const response = await itemService.get({ isOnSale: true });
+    setItemsOnSale(response.items);
+    setIsLoading(false);
+  };
 
-    const fetchData = async () => {
-        setIsLoading(true);
-        const response = await itemService.get({ 'isOnSale': true });
-        setItemsOnSale(response.items)
-        setIsLoading(false);
-    };
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(() => {
-        fetchData();
-    }, []);
+  //console.log(itemsOnSale)
 
-    //console.log(itemsOnSale)
-
-    return (
-        <div>
-        {
-            isLoading ? <p>Loading...</p> : 
-            itemsOnSale.length !== 0 ? <ProductsList items={itemsOnSale} /> :
-                'Currenty there are no items on sale :('
-        }
-        </div>
-    );
-}
+  return (
+    <div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : itemsOnSale.length !== 0 ? (
+        <ProductsList items={itemsOnSale} />
+      ) : (
+        "Currenty there are no items on sale :("
+      )}
+    </div>
+  );
+};
 
 export default Deals;
