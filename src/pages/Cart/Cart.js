3e7673ee6de50c50ed482cba407063f721e0c2a2
kamil <rba.kamil@gmail.com>
2021-12-22 13:41:47 +0100

    change indentations

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -1,39 +1,43 @@
-import { useCart } from '../../context/CartContext';
-import CartItem from '../../components/CartItem/CartItem';
-import Button from '../../components/Button/Button';
-import { Link } from 'react-router-dom';
-import './Cart.css';
+import { useCart } from "../../context/CartContext";
+import CartItem from "../../components/CartItem/CartItem";
+import Button from "../../components/Button/Button";
+import { Link } from "react-router-dom";
+import "./Cart.css";
 
 const Cart = () => {
-    const { cart, getTotal } = useCart();
-    const renderCart = () => {
-        return (
-            <div>
-                <h2 className='cart-header'>Shopping Cart</h2>
-                <div className='cart-container'>
-                    { cart.map(item => <div className='cart-item'> <CartItem item={ item }/> </div>)}
-                    <footer className='cart-summary'>
-                        <Link to='/checkout'>
-                            {/* <button>Checkout</button> */}
-                            <Button label='Checkout'/>
-                        </Link>
-                        <h2>${ getTotal() }</h2>
-                    </footer>
-                    
-                </div>
-            </div>
-        )
-    }
-
+  const { cart, getTotal } = useCart();
+  const renderCart = () => {
     return (
-        <div>
-            {
-                cart.length === 0 ? 
-                <h2 className='empty-cart-info'> Your cart is empty </h2> :
-                renderCart()
-            }      
-        </div>       
+      <div>
+        <h2 className="cart-header">Shopping Cart</h2>
+        <div className="cart-container">
+          {cart.map((item) => (
+            <div className="cart-item">
+              {" "}
+              <CartItem item={item} />{" "}
+            </div>
+          ))}
+          <footer className="cart-summary">
+            <Link to="/checkout">
+              {/* <button>Checkout</button> */}
+              <Button label="Checkout" />
+            </Link>
+            <h2>${getTotal()}</h2>
+          </footer>
+        </div>
+      </div>
     );
-}
+  };
+
+  return (
+    <div>
+      {cart.length === 0 ? (
+        <h2 className="empty-cart-info"> Your cart is empty </h2>
+      ) : (
+        renderCart()
+      )}
+    </div>
+  );
+};
 
 export default Cart;
