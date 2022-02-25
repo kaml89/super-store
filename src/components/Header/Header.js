dc6710f0aec568896df3f709a17eef592b0d65eb
kamil <rba.kamil@gmail.com>
2022-02-25 18:38:37 +0100

    move items table to separate component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,21 +3,23 @@ import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
 import AddCircle from "@mui/icons-material/AddCircle";
 
-const TableToolbar = ({ handleClick }) => {
+const Header = ({ handleClick = null, label, isButton = false }) => {
   return (
     <Toolbar variant="dense" sx={{ backgroundColor: "white" }}>
       <Typography variant="h6" component="div" sx={{ flex: "1 1 100%" }}>
-        Store items
+        {label}
       </Typography>
-      <Button
-        onClick={() => handleClick()}
-        variant="contained"
-        startIcon={<AddCircle />}
-      >
-        Add
-      </Button>
+      {isButton ? (
+        <Button
+          onClick={() => handleClick()}
+          variant="contained"
+          startIcon={<AddCircle />}
+        >
+          Add
+        </Button>
+      ) : null}
     </Toolbar>
   );
 };
 
-export default TableToolbar;
+export default Header;
--- src/pages/AdminDashboard/Items/Items.js
+++ src/pages/AdminDashboard/Items/Items.js
@@ -12,103 +12,19 @@ import DeleteIcon from "@mui/icons-material/Delete";
 import IconButton from "@mui/material/IconButton";
 import EditIcon from "@mui/icons-material/Edit";
 import ItemFormDialog from "../../../components/ItemFormDialog/ItemFormDialog";
-import TableToolbar from "../../../components/TableToolbar/TableToolbar";
+import Header from "../../../components/Header/Header";
 import itemService from "../../../services/item.service";
 import useItems from "../../../queries/item/useItems";
 import useDeleteItem from "../../../queries/item/useDeleteItem";
 import CustomButton from "../../../components/Button/Button";
+import ItemsList from "../../../components/AdminDashboard/ItemsList/ItemsList";
 
-const ItemsTable = () => {
-  const { status, data, error, isLoading } = useItems();
-  const [isOpen, setIsOpen] = useState(false);
-  const [currentItem, setCurrentItem] = useState({});
-  const deleteItem = useDeleteItem();
-
-  const handleClose = () => {
-    setCurrentItem({});
-    setIsOpen(false);
-  };
-
-  const handleOpen = (item = {}) => {
-    setCurrentItem(item);
-    setIsOpen(true);
-  };
-
+const Items = () => {
   return (
-    <>
-      {isLoading ? (
-        <p>Loading</p>
-      ) : (
-        <>
-          <TableToolbar handleClick={handleOpen} />
-          <TableContainer component={Paper} sx={{ borderRadius: 0 }}>
-            <Table
-              sx={{ minWidth: 650 }}
-              size="small"
-              aria-label="a dense table"
-            >
-              <TableHead>
-                <TableRow>
-                  <TableCell>Name</TableCell>
-                  <TableCell>Price</TableCell>
-                  <TableCell>Stock Count</TableCell>
-                  <TableCell>Description</TableCell>
-                  <TableCell>On Sale</TableCell>
-                  <TableCell>Actions</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {data.map((item) => (
-                  <TableRow
-                    hover
-                    key={item.id}
-                    sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-                  >
-                    <TableCell
-                      component="th"
-                      scope="row"
-                      onClick={() => handleOpen(item)}
-                      sx={{ cursor: "pointer" }}
-                    >
-                      {item.name}
-                    </TableCell>
-                    <TableCell>{item.price}</TableCell>
-                    <TableCell>{item.stockCount}</TableCell>
-                    <TableCell>
-                      <Box
-                        sx={{
-                          maxWidth: "500px",
-                          whiteSpace: "nowrap",
-                          overflow: "hidden",
-                          textOverflow: "ellipsis",
-                        }}
-                      >
-                        {item.description}
-                      </Box>
-                    </TableCell>
-                    <TableCell>{String(item.isOnSale)}</TableCell>
-                    <TableCell>
-                      <IconButton onClick={() => handleOpen(item)}>
-                        <EditIcon />
-                      </IconButton>
-                      <IconButton onClick={() => deleteItem.mutate(item.id)}>
-                        <DeleteIcon />
-                      </IconButton>
-                    </TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
-          <ItemFormDialog
-            open={isOpen}
-            handleClose={handleClose}
-            item={currentItem}
-          />
-        </>
-      )}
-    </>
+    <div>
+      <ItemsList />
+    </div>
   );
 };
 
-export default ItemsTable;
+export default Items;
