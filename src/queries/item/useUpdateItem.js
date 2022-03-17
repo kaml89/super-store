dee247935dd734fe4ba35a84df8823ae9b4b6fc9
kamil <rba.kamil@gmail.com>
2022-03-17 19:31:15 +0100

    fix query invalidation

--- src/queries/item/useUpdateItem.js
+++ src/queries/item/useUpdateItem.js
@@ -1,11 +1,11 @@
 import { useMutation, useQueryClient } from "react-query";
 import itemService from "../../services/item.service";
 
-const useUpdateItem = () => {
+const useUpdateItem = (itemId) => {
   const queryClient = useQueryClient();
   return useMutation((updatedItem) => itemService.update(updatedItem), {
     onSuccess: () => {
-      queryClient.invalidateQueries(["items"]);
+      queryClient.invalidateQueries(["items", itemId]);
     },
   });
 };
