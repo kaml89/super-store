34fb4f65597bd0fc918d2d17fedf927526d55530
kamil <rba.kamil@gmail.com>
2022-03-18 20:07:50 +0100

    move providers to separate file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import React, { Suspense, lazy } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import AppProvider from "./providers/index";
-// import { ReactQueryDevtools } from "react-query/devtools";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Cart from "./pages/Cart/Cart";
 import Deals from "./pages/Deals/Deals";
 import Item from "./components/Item/Item";
 import Checkout from "./pages/Checkout/Checkout";
-import { CartProvider } from "./context/CartContext";
-import { NotificationProvider } from "./context/NotificationContext";
-import { AuthProvider } from "./context/AuthContext";
 import Navigation from "./components/Navigation/Navigation";
-import Notification from "./components/Notification/Notification";
+import LoadingScreen from "./components/LoadingScreen/LoadingScreen";
 import Signup from "./pages/Signup/Signup";
 import Login from "./pages/Login/Login";
 import { AdminRoute } from "./routes/PrivateRoutes";
-import { ThemeProvider } from "@mui/material/styles";
-import { theme } from "./themes/theme";
 
 const AdminDashboard = lazy(() =>
   import("./pages/AdminDashboard/AdminDashboard")
@@ -28,17 +22,10 @@ function App() {
 
   return (
     <div className="App">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense fallback={<LoadingScreen />}>
         <Router>
           <AppProvider>
-            {/* <QueryClientProvider client={queryClient}>
-              <AuthProvider>
-                <CartProvider>
-                  <ThemeProvider theme={theme}>
-                    <NotificationProvider> */}
             <Navigation />
-
-            {/* <Notification /> */}
             <Switch>
               <Route exact path="/" component={Home} />
               <Route path="/deals" component={Deals} />
@@ -51,12 +38,6 @@ function App() {
                 <AdminDashboard />
               </AdminRoute>
             </Switch>
-            {/* </NotificationProvider>
-                  </ThemeProvider>
-                </CartProvider>
-              </AuthProvider> */}
-            {/* <ReactQueryDevtools initialIsOpen /> */}
-            {/* </QueryClientProvider> */}
           </AppProvider>
         </Router>
       </Suspense>
