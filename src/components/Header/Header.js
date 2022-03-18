2d529d104ec729c99cf16f34506980c7120e3ddd
kamil <rba.kamil@gmail.com>
2022-03-18 20:14:45 +0100

    add propTypes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import Typography from "@mui/material/Typography";
 import Toolbar from "@mui/material/Toolbar";
 import Button from "@mui/material/Button";
@@ -5,8 +6,8 @@ import AddCircle from "@mui/icons-material/AddCircle";
 
 const Header = ({ handleClick = null, label, isButton = false }) => {
   return (
-    <Toolbar variant="dense" sx={{ backgroundColor: "white" }}>
-      <Typography variant="h6" component="div" sx={{ flex: "1 1 100%" }}>
+    <Toolbar variant="dense" sx={{ backgroundColor: "white", padding: "20px" }}>
+      <Typography variant="h3" component="div" sx={{ flex: "1 1 100%" }}>
         {label}
       </Typography>
       {isButton ? (
@@ -20,6 +21,11 @@ const Header = ({ handleClick = null, label, isButton = false }) => {
       ) : null}
     </Toolbar>
   );
+};
+Header.propTypes = {
+  handleClick: PropTypes.func,
+  label: PropTypes.string.isRequired,
+  isBUtton: PropTypes.bool,
 };
 
 export default Header;
